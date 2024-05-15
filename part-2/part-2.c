b710d82e816c3ec2481754461e422691cdfb9e67
ophirpap <ophir7@gmail.com>
2024-05-16 01:20:31 +0300

    part-2

--- part-2/part-2.c
+++ part-2/part-2.c
@@ -1,16 +1,35 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+int mystrlen (char *str){
+	if(str == (char*)NULL)
+		return 0;
+	int i=0;
+	while (str[i]!=(char)0)
+		i++;
+	return i;
+}
+
+void mystrcpy (char* trg, char* src){
+	if(src==(char*)NULL)
+		return;
+	int i=0;
+	while(src[i]!=(char)0){
+		trg[i]=src[i];
+		i++;
+	}
+	trg[i]=(char)0;
+}
 int main(int argc, char** argv){
 	bool add;
 	int size =0;
 	if(argc > 1){
 		add = (argv[1][0] == '+');
-		size = strlen(argv[1])-2;		
+		size = mystrlen(argv[1])-2;	
 	}
 	char key[size];
 	if(size > 0)
-		 strcpy(key, &argv[1][2]);// copying the rest of the string to the key  
+		 mystrcpy(key, &argv[1][2]);// copying the rest of the string to the key  
 	FILE *infile = stdin;
 	FILE *outfile = stdout;
 	int i=0;
@@ -39,10 +58,14 @@ int main(int argc, char** argv){
 					}		
 				} 
 			}				
-				i = (i== strlen(key)-1) ? 0 : i+1;
+				i = (i== mystrlen(key)-1) ? 0 : i+1;
 		}
 		else
 			fputc(c,outfile);
 	}
 	return 0;
 }
+
+
+
+
